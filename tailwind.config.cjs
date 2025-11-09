d34f838b375e495a246cd5dba50b4907c9e0a9f2
guilisbooa <guilisboadl@gmail.com>
2025-11-09 16:58:48 -0300

    Estilização nos arquivos tailwind.config e index.css

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,5 +1,19 @@
 module.exports = {
-  content: ['./index.html', './src/**/*.{ts,tsx}'],
-  theme: { extend: {} },
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#1E40AF", // azul escuro
+        secondary: "#3B82F6", // azul médio
+        accent: "#FBBF24", // amarelo
+        light: "#F9FAFB",
+        dark: "#1F2937",
+      },
+      fontFamily: {
+        sans: ["Poppins", "sans-serif"],
+        title: ["Montserrat", "sans-serif"],
+      },
+    },
+  },
   plugins: [],
-}
+};
